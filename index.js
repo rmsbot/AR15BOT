1ff109aab3ba1856f339db32ee0833af283e352d
RmsBOT7 <75308262+RmsBOT7@users.noreply.github.com>
2020-12-10 22:17:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const aris2 = require("./lib/aris2.js");
 const aris3 = require("./lib/aris3.js");
 const aris4 = require("./lib/aris4.js");
 const menuall = require("./lib/menuall.js");
+const copas = require("./lib/copas.js");
 const itsmeiky = require("./lib/itsmeiky.js");
 const readTextInImage = require('./lib/ocr')
 //A187ID
@@ -181,6 +182,9 @@ conn.sendMessage(id, ' *Iya Memek Lo Bau - BOT!!!* ' ,MessageType.text);
 }
 else if (text == 'Memek'){
 conn.sendMessage(id, ' *Gw Bilang Apa Memek Lo Yang Bau - BOT!!!* ' ,MessageType.text);
+}
+else if (text == '?copas'){
+conn.sendMessage(id, ' *Nih Gan Kalau Mau Tau Arti Kata *Copas* Thanks!!!* ' ,MessageType.text);
 }
 else if (text == '!selamat'){
 conn.sendMessage(id, ' *───❉ FRM BOT ❉──*\n Yeah. Dapat tulisan selamat ' ,MessageType.text);
@@ -825,6 +829,43 @@ switch(bulan) {
 var tampilTanggal = "TANGGAL: " + hari + ", " + tanggal + " " + bulan + " " + tahun;
 var tampilWaktu = "JAM: " + jam + ":" + menit + ":" + detik;
 conn.sendMessage(id, aris1.aris1(id, A187, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwhatsapp, youtube) ,MessageType.text);
+}
+if (text == '?copas'){
+const corohelp = await get.get('https://covid19.mathdro.id/api/countries/id').json()
+var date = new Date();
+var tahun = date.getFullYear();
+var bulan = date.getMonth();
+var tanggal = date.getDate();
+var hari = date.getDay();
+var jam = date.getHours();
+var menit = date.getMinutes();
+var detik = date.getSeconds();
+switch(hari) {
+ case 0: hari = "Minggu"; break;
+ case 1: hari = "Senin"; break;
+ case 2: hari = "Selasa"; break;
+ case 3: hari = "Rabu"; break;
+ case 4: hari = "Kamis"; break;
+ case 5: hari = "Jum at"; break;
+ case 6: hari = "Sabtu"; break;
+}
+switch(bulan) {
+ case 0: bulan = "Januari"; break;
+ case 1: bulan = "Februari"; break;
+ case 2: bulan = "Maret"; break;
+ case 3: bulan = "April"; break;
+ case 4: bulan = "Mei"; break;
+ case 5: bulan = "Juni"; break;
+ case 6: bulan = "Juli"; break;
+ case 7: bulan = "Agustus"; break;
+ case 8: bulan = "September"; break;
+ case 9: bulan = "Oktober"; break;
+ case 10: bulan = "November"; break;
+ case 11: bulan = "Desember"; break;
+}
+var tampilTanggal = "TANGGAL: " + hari + ", " + tanggal + " " + bulan + " " + tahun;
+var tampilWaktu = "JAM: " + jam + ":" + menit + ":" + detik;
+conn.sendMessage(id, copas.copas(id, A187, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwhatsapp, youtube) ,MessageType.text);
 }
 if (text == '#menu2'){
 const corohelp = await get.get('https://covid19.mathdro.id/api/countries/id').json()
