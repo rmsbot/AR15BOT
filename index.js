bdeae70b16e9a664c06d6a8dac306abc1fcc7ade
RmsBOT7 <75308262+RmsBOT7@users.noreply.github.com>
2020-12-10 20:44:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -635,7 +635,7 @@ const say = (`*───❉ITSMEIKY BOT❉──*\n Makasih. Anda menilai \n *${
 if (text.includes("!tinyurl")){
 const teks = text.replace(/!tinyurl /, "")
 axios.get(`https://tinyurl.com/api-create.php?url=${teks}`).then((res) => {
-	conn.sendMessage(id,  ───❉ FRM BOT ❉──\n [WAIT] Proses...❗ , MessageType.text)
+	conn.sendMessage(id,' ───❉ FRM BOT ❉──\n [WAIT] Proses...❗', MessageType.text)
     let hasil = ` ───❉ITSMEIKY BOT❉──\n *Link yang diperpendek dengan tinyurl* \n\n *link:* _${res.data}_ `;
     conn.sendMessage(id, hasil ,MessageType.text);
 })
