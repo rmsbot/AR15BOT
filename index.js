ee91de8e0c950b0a539e1c05674ebf524b315b12
RmsBOT7 <75308262+RmsBOT7@users.noreply.github.com>
2020-12-13 13:16:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -374,7 +374,7 @@ axios.get(`http://inspirobot.me/api?generate=true`).then((res) => {
 }
 if (text.includes("!simi")){
 const teks = text.replace(/%s /, "")
-axios.get(`https://arugaz.herokuapp.com/api/simisimi?kata=${teks}&apikey=DbSTTC1C0kcJaN4hQFpMaUcn3Wq_EwhZI32wesHq`).then((res) => {
+axios.get(`https://arugaz.herokuapp.com/api/simisimi?kata=${teks}&apikey=7q-Ybzcd2JJ8Sut7tfF4.GHw18-uO2-PRK0RVk8y`).then((res) => {
     let hasil = ` \n\n _${res.data.result}_ `;
     conn.sendMessage(id, hasil ,MessageType.text);
 })
